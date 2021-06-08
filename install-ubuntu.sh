d3ffcd4ce0638376c7f1db9f675f3709f721d064
koushik-ms <koushik.ms@gmail.com>
2021-06-08 17:49:13 +0000

    extend install script to download simulator

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -12,3 +12,13 @@ cd ..
 cd ..
 sudo ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
 sudo rm -r uWebSockets
+
+# install simulator
+mkdir simulator
+cd simulator
+wget https://github.com/udacity/self-driving-car-sim/releases/download/T3_v1.2/term3_sim_linux.zip
+unzip term3_sim_linux.zip
+cd term3_sim_linux
+chmod +x term3_sim.$(uname -m)
+cd ..
+cd ..
